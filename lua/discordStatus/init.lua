9afc65edde37c8301935c42dfdae6d53e95dd541
Scott Osteen <osteensco@gmail.com>
2024-11-08 16:54:28 -0600

    working prototype

--- lua/discordStatus/init.lua
+++ lua/discordStatus/init.lua
@@ -1,54 +1,111 @@
 local M = {}
 
-local authToken = os.getenv("DISCORDTOKEN")
+M.config = {
+    discordEnvVariable = "DISCORDTOKEN",
+    openStatus = { "Programming", "Coding" },
+    -- TODO
+    -- handle nil and make sure it works (should clear a custom status)
+    closeStatus = { "!Programming", "Procrastinating", nil }
 
-local stdout = vim.uv.new_pipe()
+}
+
+local function wait_for_env_var()
+    -- TODO
+    -- have user pass in env variable and this function will search for it
+    -- clean up unecessary stuff like stdout varaible, print statements, dependency setup, etc
+    -- set up logging? at least allocate places where logging would be useful
+
+    local poll_timer = vim.uv.new_timer()
+
+    poll_timer:start(0, 100, vim.schedule_wrap(function()
+        local authToken = os.getenv("DISCORDTOKEN")
+
+        -- Check if the environment variable is set
+        if authToken ~= nil and authToken ~= "" then
+            -- Set the global authToken
+            M.config.discordAuthToken = authToken
+            print("DISCORDTOKEN found! ", authToken)
+
+            -- Stop the timer once the variable is found
+            poll_timer:stop()
+        else
+            -- You can log or notify that it's still not found
+            vim.notify("Waiting...")
+        end
+    end))
+end
 
 local function set_status(status_text)
-    local handle = vim.uv.spawn("curl", {
-        args = {
-            "-X", "PATCH",
-            "https://discord.com/api/v9/users/@me/settings",
-            "-H", string.format("Authorization: %s", authToken),
-            "-H", "Content-Type: application/json",
-            "-d", string.format('{"status": "online", "custom_status": {"text": "%s"}}', status_text)
-        },
-        stdio = { nil, stdout, nil }
+    local poll_timer = vim.uv.new_timer()
 
-    }, function(code, signal)
-        print("exit code", code)
-        if code ~= 0 then
-            print("Error sending status to Discord:", code, signal)
+    poll_timer:start(0, 100, vim.schedule_wrap(function()
+        local token = M.config.discordAuthToken
+        if token ~= nil and token ~= "" then
+            print(token)
+            local stdout = vim.uv.new_pipe(false)
+            local handle, err = vim.uv.spawn("curl", {
+                args = {
+                    "-X", "PATCH",
+                    "https://discord.com/api/v9/users/@me/settings",
+                    "-H", string.format("Authorization: %s", M.config.discordAuthToken),
+                    "-H", "Content-Type: application/json",
+                    "-d", string.format('{"status": "online", "custom_status": {"text": "%s"}}', status_text)
+                },
+                stdio = { nil, stdout, nil }
+
+            }, function(code, signal)
+                if code ~= 0 then
+                    vim.notify("Error sending status to Discord: " .. code .. " " .. signal)
+                else
+                    vim.notify("Status update sent successfully.")
+                end
+                stdout:close()
+                handle:close()
+            end)
+
+            if not handle then
+                vim.notify("Failed to spawn curl process: " .. err)
+                return
+            end
+
+            vim.uv.read_start(stdout, function(err, data)
+                if err then
+                    print("Error reading response: " .. err)
+                    return
+                end
+                if data then
+                    print("Response: " .. data)
+                    print("token used: ", M.config.discordAuthToken)
+                else
+                    stdout:close()
+                    handle:close()
+                end
+            end)
+
+            poll_timer:stop()
+        else
+            print("Waiting...")
         end
-    end)
-    vim.uv.read_start(stdout, function(err, data)
-        assert(not err, err)
-        if data then
-            vim.notify("Response: ", data)
-        end
-    end)
-    stdout:close()
-    handle:close()
+    end))
 end
-
 
 function M.on_startup()
-    set_status("Programming")
+    set_status("Programming", token)
 end
 
-function M.on_exit()
+function M.on_exit(token)
     set_status("Offline")
 end
 
 function M.setup()
-    M.on_startup()
-
-
     vim.api.nvim_create_autocmd("VimLeavePre", {
         callback = function()
             M.on_exit()
         end,
     })
+
+    wait_for_env_var()
+    M.on_startup()
 end
 
 return M
---
+++ plugin/discordStatus.lua
@@ -0,0 +1,2 @@
+-- print("plugin lua file")
+-- require('dotenv').setup({ enable_on_load = true, verbose = true, })
