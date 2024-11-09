7d45a8dcd4e9ad4d7c6c9a5d117023bc4e2cc152
Scott Osteen <osteensco@gmail.com>
2024-11-08 22:18:09 -0600

    rm verbose from dotenv

--- plugin/discordStatus.lua
+++ plugin/discordStatus.lua
@@ -1,2 +1,2 @@
-require('dotenv').setup({ enable_on_load = true, verbose = true, wk_dir = vim.fn.stdpath("config") })
+require('dotenv').setup({ enable_on_load = true, verbose = false, wk_dir = vim.fn.stdpath("config") })
 require('discordStatus').wait_for_env_var()
